f8248fee117367ec3c74c415e0f3abf2b2efc853
Edith707 <edith.lopezhuerta@gmail.com>
2018-07-30 19:05:08 -0500

    agregando cars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   nombre = "Edith";
+
+  visible: boolean = false;
+  /*any cualquier tipo*/
+
+  cars: any[] = [
+  "toyota", "nissan", "ford", "tesla"
+  ]
+
+  autos:any[]= [
+    {id:1, nombre: "ford", total:0, colores:["azul", "verde", "gris"]},
+    {id:2, nombre: "toyota", total:2, colores:["negro", "rojo", "blanco"]}
+  ]
+
 }
